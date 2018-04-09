b8abeba95566da6aff858b6679cfe70cf6c1b428
Gnegon <matgrz1993@gmail.com>
2018-04-09 21:11:53 +0200

    Agora Navigator v0.95
    - new tasks added,
    - TaskDetailView graphics changed,
    - id and pin padding in closeTask added,
    - case-insensitivity when checkin task impl

--- AgoraNavigator/AgoraNavigator/Tasks/GameTask.cs
+++ AgoraNavigator/AgoraNavigator/Tasks/GameTask.cs
@@ -12,7 +12,8 @@ namespace AgoraNavigator.Tasks
         public enum TaskType
         {
             Text = 0,
-            Button = 1
+            Button = 1,
+            LongText = 2
         }
 
         public enum TaskStatus
@@ -35,6 +36,10 @@ namespace AgoraNavigator.Tasks
 
         public string correctAnswer { get; set; }
 
+        public List<string> correctAnswers { get; set; }
+
+        public int minimumCorrectAnswers { get; set; }
+
         public int scorePoints { get; set; }
 
         public TaskStatus taskStatus { get; set; }
@@ -48,7 +53,9 @@ namespace AgoraNavigator.Tasks
             bool result = false;
             GameTask closedTask = allTasks[taskId];
             Console.WriteLine("GameTask:closeTask:task.id=" + closedTask.id);
-            String databasePath = "/users/" + Users.loggedUser.Id + "/" + Users.loggedUser.Pin + "/tasks/";
+            String idDbPath = Users.loggedUser.Id.ToString().PadLeft(4, '0');
+            String pinDbPath = Users.loggedUser.Pin.ToString().PadLeft(4, '0');
+            String databasePath = "/users/" + idDbPath + "/" + pinDbPath + "/tasks/";
             UserTasksInDb userTasksInDb = new UserTasksInDb();
             userTasksInDb.totalPoints = Users.loggedUser.TotalPoints + closedTask.scorePoints;
             userTasksInDb.closedTasks = new List<int>();
@@ -126,7 +133,7 @@ namespace AgoraNavigator.Tasks
                 title = "More than one animal is?",
                 description = "Ask Agora organizer what is the name of the AEGEE-Cracow sheep",
                 taskType = TaskType.Text,
-                correctAnswer = "Mateusz",
+                correctAnswer = "mateusz",
                 scorePoints = 2,
                 taskStatus = TaskStatus.NotStarted,
                 needBluetoothAndLocation = false
@@ -184,7 +191,7 @@ namespace AgoraNavigator.Tasks
                 title = "Randevu",
                 description = "Talk with AEGEE-Cracow president and ask him for secret password",
                 taskType = TaskType.Text,
-                correctAnswer = "Buka",
+                correctAnswer = "buka",
                 scorePoints = 2,
                 taskStatus = TaskStatus.NotStarted,
                 needBluetoothAndLocation = false
@@ -195,7 +202,7 @@ namespace AgoraNavigator.Tasks
                 title = "Women WC",
                 description = "Enter code from Women's WC",
                 taskType = TaskType.Text,
-                correctAnswer = "Closed",
+                correctAnswer = "closed",
                 scorePoints = 2,
                 taskStatus = TaskStatus.NotStarted,
                 needBluetoothAndLocation = false
@@ -206,7 +213,7 @@ namespace AgoraNavigator.Tasks
                 title = "Plenary code",
                 description = "Enter code from Plenary",
                 taskType = TaskType.Text,
-                correctAnswer = "Code",
+                correctAnswer = "code",
                 scorePoints = 2,
                 taskStatus = TaskStatus.NotStarted,
                 needBluetoothAndLocation = false
@@ -217,7 +224,7 @@ namespace AgoraNavigator.Tasks
                 title = "Polish favourite program",
                 description = "Find out what Polish people watch on sunday at dinner time",
                 taskType = TaskType.Text,
-                correctAnswer = "Familiada",
+                correctAnswer = "familiada",
                 scorePoints = 2,
                 taskStatus = TaskStatus.NotStarted,
                 needBluetoothAndLocation = false
@@ -235,7 +242,7 @@ namespace AgoraNavigator.Tasks
             });
             allTasks.Add(new GameTask
             {
-                id = 13,
+                id = 14,
                 title = "I need a dollar dollar...",
                 description = "Lend from AEGEEans five different currency in banknote or coin and show them to local organizer",
                 taskType = TaskType.Button,
@@ -243,6 +250,320 @@ namespace AgoraNavigator.Tasks
                 taskStatus = TaskStatus.NotStarted,
                 needBluetoothAndLocation = false,
                 dbName = "Dollar"
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 15,
+                title = "Gunnar Quiz - question #1",
+                description = "How many times did AEGEE-Kraków sign the CdA?",
+                taskType = TaskType.LongText,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswers = new List<string>
+                {
+                    "2",
+                    "twice",
+                    "two"
+                },
+                minimumCorrectAnswers = 1
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 16,
+                title = "Gunnar Quiz - question #2",
+                description = "When and where did the last Agora in Poland take place?",
+                taskType = TaskType.LongText,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswers = new List<string>
+                {
+                    "2006",
+                    "warsaw"
+                },
+                minimumCorrectAnswers = 2
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 17,
+                title = "Gunnar Quiz - question #3",
+                description = "How many Agoras took place so far, including this one?",
+                taskType = TaskType.Text,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswer = "65"
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 18,
+                title = "Gunnar Quiz - question #4",
+                description = "Which antenna has been the only one to organise four Agoras?",
+                taskType = TaskType.Text,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswer = "aegee-enschede"
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 19,
+                title = "Gunnar Quiz - question #5",
+                description = "How many AEGEE members applied for the Agora?",
+                taskType = TaskType.Text,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswer = "1207"
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 20,
+                title = "Gunnar Quiz - question #6",
+                description = "How many members had the CDs maximum until 1996?",
+                taskType = TaskType.Text,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswer = "16"
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 21,
+                title = "Gunnar Quiz - question #7",
+                description = "How many antennae and contact antennae ever existed?",
+                taskType = TaskType.Text,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswer = "400"
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 22,
+                title = "Gunnar Quiz - question #8",
+                description = "What is the mascot of AEGEE-Aachen?",
+                taskType = TaskType.Text,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswer = "polar bear"
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 23,
+                title = "Gunnar Quiz - question #9",
+                description = "What was the name of the project that used to present funny films at the Agora?",
+                taskType = TaskType.Text,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswer = "aegee.tv"
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 24,
+                title = "Gunnar Quiz - question #10",
+                description = "What did participants get during Agora Zaragoza 2013?",
+                taskType = TaskType.LongText,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswers = new List<string>
+                {
+                    "lindt",
+                    "chocolate"
+                },
+                minimumCorrectAnswers = 2
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 25,
+                title = "Gunnar Quiz - question #11",
+                description = "What did AEGEE vote in favour of introducing at Agora Poznan 1999?",
+                taskType = TaskType.Text,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswer = "enlightened despotism"
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 26,
+                title = "Gunnar Quiz - question #12",
+                description = "Which AEGEE-Europe member married his Secretary?",
+                taskType = TaskType.Text,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswer = "christoph strohm"
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 27,
+                title = "Gunnar Quiz - question #13",
+                description = "What was Agora Montpellier 1994 famous for?",
+                taskType = TaskType.LongText,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswers = new List<string>
+                {
+                    "elected",
+                    "president",
+                    "outside"
+                },
+                minimumCorrectAnswers = 2
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 28,
+                title = "Gunnar Quiz - question #14",
+                description = "Which drastic life change happened to former AEGEE-Europe President Georg von der Gablentz?",
+                taskType = TaskType.LongText,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswers = new List<string>
+                {
+                    "changed",
+                    "gender",
+                    "woman"
+                },
+                minimumCorrectAnswers = 2
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 29,
+                title = "Gunnar Quiz - question #15",
+                description = "What was the third AEGEE-Europe office originally?",
+                taskType = TaskType.LongText,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswers = new List<string>
+                {
+                    "museum",
+                    "underwear"
+                },
+                minimumCorrectAnswers = 2
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 30,
+                title = "Gunnar Quiz - question #16",
+                description = "Which animals lived in the backyard of that office?",
+                taskType = TaskType.Text,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswer = "chickens"
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 31,
+                title = "Gunnar Quiz - question #17",
+                description = "Which AEGEE antenna has its own pub?",
+                taskType = TaskType.Text,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswer = "aegee-enschede"
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 32,
+                title = "Gunnar Quiz - question #18",
+                description = "How old was the oldest President of AEGEE - Europe when he was elected?",
+                taskType = TaskType.LongText,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswers = new List<string>
+                {
+                    "30"
+                },
+                minimumCorrectAnswers = 1
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 33,
+                title = "Gunnar Quiz - question #19",
+                description = "Which Working Group moved a lot of people when it was founded in 2001?",
+                taskType = TaskType.LongText,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswers = new List<string>
+                {
+                    "dance"
+                },
+                minimumCorrectAnswers = 1
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 34,
+                title = "Gunnar Quiz - question #20",
+                description = "How did AEGEE-Bucuresti transport 500 Agora participants to their accommodation?",
+                taskType = TaskType.LongText,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswers = new List<string>
+                {
+                    "train",
+                    "constanta"
+                },
+                minimumCorrectAnswers = 2
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 35,
+                title = "Gunnar Quiz - question #21",
+                description = "Where were the participants of Agora Izmir lodged in 2005?",
+                taskType = TaskType.LongText,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswers = new List<string>
+                {
+                    "five",
+                    "hotel"
+                },
+                minimumCorrectAnswers = 2
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 36,
+                title = "Gunnar Quiz - question #22",
+                description = "Where were AEGEE-Leiden's Agora participants lodged in 2010?",
+                taskType = TaskType.LongText,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswers = new List<string>
+                {
+                    "outside",
+                    "tents"
+                },
+                minimumCorrectAnswers = 2
+            });
+            allTasks.Add(new GameTask
+            {
+                id = 37,
+                title = "Gunnar Quiz - question #23",
+                description = "What happened in the garage next to the Agora gym in Warsaw in 2006?",
+                taskType = TaskType.LongText,
+                scorePoints = 1,
+                taskStatus = TaskStatus.NotStarted,
+                needBluetoothAndLocation = false,
+                correctAnswers = new List<string>
+                {
+                    "sex",
+                    "camera"
+                },
+                minimumCorrectAnswers = 2
             });
         }
     }
--- AgoraNavigator/AgoraNavigator/Tasks/TaskDetailView.cs
+++ AgoraNavigator/AgoraNavigator/Tasks/TaskDetailView.cs
@@ -22,22 +22,19 @@ namespace AgoraNavigator.Tasks
             BackgroundColor = AgoraColor.DarkBlue;
             actualTask = task;
 
-            Grid grid = new Grid
+            StackLayout layout = new StackLayout
             {
-                Padding = 0,
-                RowSpacing = 0
-            };
-
-            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
-            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(11, GridUnitType.Star) });
-            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) });
-            grid.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
-
-            BoxView boxView = new BoxView
-            {
+                Margin = new Thickness(0, 0),
                 BackgroundColor = AgoraColor.Blue
             };
+
+            StackLayout layoutTop = new StackLayout
+            {
+                Margin = new Thickness(20, 20),
+                BackgroundColor = AgoraColor.Blue
+            };
+            layout.Children.Add(layoutTop);
+
             Label titleLabel = new Label
             {
                 Text = task.title,
@@ -54,30 +51,31 @@ namespace AgoraNavigator.Tasks
                 FontFamily = AgoraFonts.GetPoppinsMedium(),
                 FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)),
             };
+            layoutTop.Children.Add(titleLabel);
+            layoutTop.Children.Add(descriptionLabel);
 
-            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) }); // 0 - Field above Title
-            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(3, GridUnitType.Star) }); // 1 - Title
-            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(5, GridUnitType.Star) }); // 2 - Description
-            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(4, GridUnitType.Star) }); // 3 - Field bet Desc and Answer Label
+            Grid gridBottom = new Grid
+            {
+                Margin = new Thickness(10, 30),
+            };
 
-            Grid.SetRowSpan(boxView, 3);
-            Grid.SetColumnSpan(boxView, 5);
-            Grid.SetColumnSpan(titleLabel, 3);
-            Grid.SetColumnSpan(descriptionLabel, 3);
+            gridBottom.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
+            gridBottom.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(4, GridUnitType.Star) });
+            gridBottom.ColumnDefinitions.Add(new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) });
 
-            grid.Children.Add(boxView, 0, 5, 0, 3);
-            grid.Children.Add(titleLabel, 1, 4, 1, 2);
-            grid.Children.Add(descriptionLabel, 1, 4, 2, 3);
+            gridBottom.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            gridBottom.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) });
+            gridBottom.RowDefinitions.Add(new RowDefinition { Height = new GridLength(100) });
 
             answerButton = new Button
             {
                 BackgroundColor = AgoraColor.Blue,
                 TextColor = AgoraColor.DarkBlue,
                 FontFamily = AgoraFonts.GetPoppinsBold(),
-                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
+                FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label))
             };
             answerButton.Clicked += OnAnswerButtonClick;
-            if (task.taskType == TaskType.Text)
+            if ((task.taskType == TaskType.Text) || (task.taskType == TaskType.LongText))
             {
                 answerButton.Text = "SEND ANSWER";
                 Label answerLabel = new Label
@@ -93,25 +91,15 @@ namespace AgoraNavigator.Tasks
                     TextColor = Color.White,
                     FontFamily = AgoraFonts.GetPoppinsMedium(),
                     FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)),
-                    VerticalOptions = LayoutOptions.End,                    
+                    VerticalOptions = LayoutOptions.End,
                     HorizontalTextAlignment = TextAlignment.Center,
                     PlaceholderColor = Color.LightGray,
                     Placeholder = "Answer"
                 };
 
-                grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(3, GridUnitType.Star) }); // 4 - Answer Label
-                grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(3, GridUnitType.Star) }); // 5 - Entry
-                grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(1, GridUnitType.Star) }); // 6 - Field bet Entry and Button
-                grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(4, GridUnitType.Star) }); // 7 - Answer Button
-                grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(8, GridUnitType.Star) }); // 8 - Rest field
-
-                BoxView boxView1 = new BoxView
-                {
-                    BackgroundColor = Color.Red
-                };
-                grid.Children.Add(answerLabel, 2, 4);
-                grid.Children.Add(answerEntry, 2, 5);
-                grid.Children.Add(answerButton, 2, 7);
+                gridBottom.Children.Add(answerLabel, 1, 0);
+                gridBottom.Children.Add(answerEntry, 1, 1);
+                gridBottom.Children.Add(answerButton, 1, 2);
             }
             else
             {
@@ -124,12 +112,18 @@ namespace AgoraNavigator.Tasks
                 {
                     answerButton.Text = "CHECK TASK";
                 }
-                    
-                grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(4, GridUnitType.Star) }); // 6 - Answer Button
-                grid.Children.Add(answerButton, 2, 4);
-                grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(8, GridUnitType.Star) }); // 7 - Rest field              
+
+                gridBottom.Children.Add(answerButton, 1, 2);
             }
-            Content = grid;
+            Content = new StackLayout
+            {
+                Margin = new Thickness(0, 0),
+                Children =
+                {
+                    layout,
+                    gridBottom
+                }
+            };
         }
 
         public async void OnAnswerButtonClick(object sender, EventArgs e)
@@ -148,9 +142,45 @@ namespace AgoraNavigator.Tasks
                     switch (actualTask.taskType)
                     {
                         case TaskType.Text:
+                            if(answerEntry.Text != null)
+                            {
+                                answerEntry.Text = answerEntry.Text.ToLower();
+                            }
                             Console.WriteLine("answerEntry.Text=" + answerEntry.Text);
                             Console.WriteLine("actualTask.correctAnswer" + actualTask.correctAnswer);
                             if (actualTask.correctAnswer == answerEntry.Text)
+                            {
+                                Console.WriteLine("Yeah! Correct answer!");
+                                await GamePage.tasksMasterPage.closeTask(actualTask);
+                            }
+                            else
+                            {
+                                SimplePopup popup = new SimplePopup("Bad answer!", "Try one more time!")
+                                {
+                                    ColorBackground = Color.Red,
+                                    ColorBody = Color.White,
+                                    ColorTitle = Color.White,
+                                };
+                                popup.SetColors();
+                                await Navigation.PushPopupAsync(popup);
+                                actualTask.taskStatus = GameTask.TaskStatus.NotStarted;
+                            }
+                            break;
+                        case TaskType.LongText:
+                            if (answerEntry.Text != null)
+                            {
+                                answerEntry.Text = answerEntry.Text.ToLower();
+                            }
+                            Console.WriteLine("answerEntry.LongText=" + answerEntry.Text);
+                            int correctAnswers = 0;
+                            foreach(string answer in actualTask.correctAnswers)
+                            {
+                                if(answerEntry.Text.Contains(answer))
+                                {
+                                    correctAnswers++;
+                                }
+                            }
+                            if(correctAnswers >= actualTask.minimumCorrectAnswers)
                             {
                                 Console.WriteLine("Yeah! Correct answer!");
                                 await GamePage.tasksMasterPage.closeTask(actualTask);
